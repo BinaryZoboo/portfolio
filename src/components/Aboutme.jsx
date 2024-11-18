536c2247d3c127c641a951f2b7edc3f28143fcbd
Nathan Dancoine <dancoinathan@gmail.com>
2024-11-18 16:47:27 +0100

    color aboutme

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -16,10 +16,14 @@ export default function Aboutme() {
 
         {/* Text Section */}
         <div className="flex w-full flex-col space-y-3 text-center sm:w-2/3 sm:text-left">
-          <p className="text-zinc-400">
-            <span className="text-zinc-500 opacity-80">{"<span>"}</span>Hey, je
-            m&apos;appelle Nathan
-            <span className="text-zinc-500 opacity-80">{"</span>"}</span>
+          <p className="dark:text-zinc-300">
+            <span className="text-green-700 opacity-80 dark:text-pink-400">
+              {"<span>"}
+            </span>
+            Hey, je m&apos;appelle Nathan
+            <span className="text-green-700 opacity-80 dark:text-pink-400">
+              {"</span>"}
+            </span>
           </p>
 
           <h1 className="text-balance text-4xl font-bold text-black md:text-5xl lg:text-6xl dark:text-white">
@@ -33,14 +37,14 @@ export default function Aboutme() {
           <div className="font-mono text-zinc-800 dark:text-zinc-300">
             <span className="opacity-60">{"<p>"}</span>
             Avec une expertise des technologies à la pointe telles que
-            <span className="text-green-700 dark:text-atlantis"> NodeJS</span>,
-            <span className="text-green-700 dark:text-atlantis"> React</span>,
-            <span className="text-green-700 dark:text-atlantis">
+            <span className="text-green-700 dark:text-pink-400"> NodeJS</span>,
+            <span className="text-green-700 dark:text-pink-400"> React</span>,
+            <span className="text-green-700 dark:text-pink-400">
               {" "}
               Javascript
             </span>
             , et
-            <span className="text-green-700 dark:text-atlantis"> Tailwind</span>
+            <span className="text-green-700 dark:text-pink-400"> Tailwind</span>
             ... Je donne des solutions web innovantes et performantes.
             <span className="opacity-60">{"</p>"}</span>
           </div>
